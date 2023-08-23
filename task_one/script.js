9a33e984b29afaa43a71fa043f1a4e6aef4767c9
Madi <madi_gaziz@mail.ru>
2023-08-23 20:37:43 +0600

    Сделал 1 задача, а именно разработал каклькулятор с массивом операции

---
+++ task_one/script.js
@@ -0,0 +1,27 @@
+let operations = ['+', '-', '*', '/'];
+let choose = prompt('Введите знак для выполнение операции')
+
+if (operations.includes(choose)) {
+    let num1 = parseFloat(prompt('Введите первое число'));
+    let num2 = parseFloat(prompt('Введите второе число'));
+
+    let result;
+    switch (choose) {
+        case '+':
+            result = num1 + num2
+            break
+        case '-':
+            result = num1 - num2
+            break
+        case '*':
+            result = num1 * num2
+            break
+        case '/':
+            result = num1 / num2
+            break
+
+    }
+    console.log(result)
+} else {
+    console.log('Неверная операция')
+}
